d383fe48fe09c9a357f406028a1b8ca033f32b35
synicaex <alexanderroskamp1@gmail.com>
2025-05-04 17:00:58 +0200

    .gitignore: added gitignore, branched before

---
+++ src/main/java/mm/physics/tutorial.java
@@ -0,0 +1,10 @@
+package mm.physics;
+
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.*;
+
+public class tutorial {
+
+    Vec2 gravity = new Vec2(0.0f, -10.0f);
+    World welt = new World(gravity);
+}
