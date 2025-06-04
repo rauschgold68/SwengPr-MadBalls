20299774b544e03fc82af5e22f9983b0407db16c
bene <arneschroedie@gmail.com>
2025-06-04 13:24:21 +0200

    actually removed the angle (rotate) problem this time

--- src/main/java/mm/model/objects/Position.java
+++ src/main/java/mm/model/objects/Position.java
@@ -6,7 +6,6 @@ package mm.model.objects;
 public class Position {
     private float x;
     private float y;
-    private float rotate;
 
     public float getX() {return this.x;}
     public void setX(float newX) {this.x = newX;}
@@ -14,6 +13,4 @@ public class Position {
     public float getY() {return this.y;}
     public void setY(float newY) {this.y = newY;}
 
-    public float getRatation() {return this.rotate;}
-    public void setRotation(float newR) {this.y = newR;}
 }
